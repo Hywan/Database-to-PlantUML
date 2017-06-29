548c2a37526d104cb3f9aef91f53d48083b74de9
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-06-29 13:48:32 +0200

    Add CommonMark backend.

---
+++ src/Backend/CommonMark/CommonMark.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hywan\DatabaseToPlantUML\Backend\CommonMark;
+
+use Hywan\DatabaseToPlantUML\Frontend;
+use Hoa\Visitor;
+
+class CommonMark implements Visitor\Visit
+{
+    public function visit(Visitor\Element $element, &$handle = null, $eldnah = null)
+    {
+        if ($element instanceof Frontend\Database) {
+            return $this->visitDatabase($element, $handle, $eldnah);
+        } elseif ($element instanceof Frontend\Table) {
+            return $this->visitTAble($element, $handle, $eldnah);
+        }
+
+        throw new \RuntimeException('Unknown element to visit ' . get_class($element) . '.');
+    }
+
+    public function visitDatabase(Frontend\Database $database, &$handle = null, &$eldnah = null): string
+    {
+        $out = '# `' . $database->name . '`' . "\n\n";
+
+        foreach ($database->tables() as $table) {
+            $out .= $table->accept($this, $handle, $eldnah) . "\n";
+        }
+
+        return $out;
+    }
+
+    public function visitTable(Frontend\Table $table, &$handle = null, &$eldnah = null): string
+    {
+        $out = '## `' . $table->name . '`' . "\n\n";
+
+        if (!empty($table->comment)) {
+            $out .= $table->comment;
+        }
+
+        $columnNames         = array_keys(get_class_vars(Frontend\Row::class));
+        $separators          = [];
+        $maximumColumnWidths = [];
+
+        foreach ($columnNames as $columnName) {
+            $maximumColumnWidths[$columnName] = strlen($columnName);
+        }
+
+        $rows = [];
+
+        foreach ($table->rows() as $row) {
+            $rows[] = $row;
+
+            foreach ($columnNames as $columnName) {
+                $maximumColumnWidths[$columnName] = max($maximumColumnWidths[$columnName], strlen($row->$columnName ?: ''));
+            }
+        }
+
+        $pattern = '';
+
+        foreach ($maximumColumnWidths as $maximumColumnWidth) {
+            $length = $maximumColumnWidth + 2;
+
+            $pattern      .= ' %-' . $length . 's |';
+            $separators[]  = str_repeat('-', $length);
+        }
+
+        $out .=
+            '|' . sprintf($pattern, ...$columnNames) . "\n" .
+            '|' . sprintf($pattern, ...$separators)  . "\n";
+
+        foreach ($rows as $row) {
+            $columnValues = array_map(
+                function ($value) {
+                    if ('' === $value || null === $value) {
+                        return '';
+                    }
+
+                    return '`' . $value . '`';
+                },
+                array_values(get_object_vars($row))
+            );
+
+            $out .= '|' . sprintf($pattern, ...$columnValues) . "\n";
+        }
+
+        return $out;
+    }
+}
