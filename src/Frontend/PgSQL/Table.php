9551a1e123ec39887bef835095c7311aa05305dd
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-09-06 14:20:19 +0200

    fix(frontend) Correct query to retrieve columns.
    Actually, the columns were correct, but the constraints were
    incorrect. Now it is fixed.

--- src/Frontend/PgSQL/Table.php
+++ src/Frontend/PgSQL/Table.php
@@ -10,15 +10,12 @@ use PDO;
 
 class Table extends Frontend\Table
 {
-    const PRIMARY = '/_pkey$/';
-
     public function columns(): iterable
     {
         $columns =
             $this->getDatabaseConnection()
                 ->prepare(
-                    'SELECT DISTINCT ' .
-                    '          c.column_name AS name, ' .
+                    'SELECT    c.column_name AS name, ' .
                     '          c.column_default AS "defaultValue", ' .
                     '          CASE c.is_nullable WHEN \'YES\' THEN 1 ELSE 0 END AS "isNullable", ' .
                     '          c.data_type AS type, ' .
@@ -32,11 +29,7 @@ class Table extends Frontend\Table
                     'AND       k.table_schema = c.table_schema ' .
                     'AND       k.column_name = c.column_name ' .
                     'LEFT JOIN information_schema.constraint_column_usage AS cc ' .
-                    'ON        cc.table_catalog = c.table_catalog ' .
-                    'AND       cc.table_schema = c.table_schema ' .
-                    'AND       cc.table_name = c.table_name ' .
-                    'AND       cc.column_name = c.column_name ' .
-                    'AND       cc.constraint_name = k.constraint_name ' .
+                    'ON        cc.constraint_name = k.constraint_name ' .
                     'WHERE     c.table_catalog = :database_name ' .
                     'AND       c.table_name = :table_name ' .
                     'AND       c.table_schema = :table_schema',
